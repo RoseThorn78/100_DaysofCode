81a6ff626fee45c9e1c8dcf8acc5b2d84dda529f
Keira Stavast <114077660+RoseThorn78@users.noreply.github.com>
2024-04-03 10:29:58 +1030

    Create main (17).py

---
+++ day 9_100 days/main (17).py
@@ -0,0 +1,22 @@
+print("Generation Generator")
+print()
+birthYear = int(input("What year were you born?: "))
+print()
+if birthYear >= 1883 and birthYear <= 1900:
+  print("Wow, you're ancient - part of the Lost Generation! What was it like to live in the late 1800s?")
+if birthYear >= 1901 and birthYear <= 1927:
+  print("Hmm, part of the Greatest Generation? What an odd name for a generation...")
+elif birthYear >= 1928 and birthYear <= 1945:
+  print("Lachlan Paul CSS")
+elif birthYear >= 1946 and birthYear <= 1964:
+  print("bluh bluh hulu lop polikololpol9")
+elif birthYear >= 1965 and birthYear <= 1980:
+  print("something anything please")
+elif birthYear >= 1981 and birthYear <= 1996:
+  print("Wow, you're a millenial?")
+elif birthYear >= 1997 and birthYear <= 2012:
+  print("So, is it true Gen Z are the most technology addicted generation? I wonder what would happen if I took away your phone...")
+elif birthYear >= 2012 and birthYear <= 2025:
+  print("Born in Gen Alpha? Yeesh, you're still a child, fella! I wonder what you're generation will turn out like...")
+else:
+  print("Try again!")
