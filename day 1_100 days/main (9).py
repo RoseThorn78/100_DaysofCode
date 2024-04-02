75321fbdfca81765fe7923e069ab694ee1c396a1
Keira Stavast <114077660+RoseThorn78@users.noreply.github.com>
2024-04-03 09:56:13 +1030

    Create main (9).py

---
+++ day 1_100 days/main (9).py
@@ -0,0 +1,7 @@
+print("Keira")
+print("February 25th 2023")
+print("""I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding in the middle of a field!""")
+print("I am feeling 😄")
+print("You can follow my progress at replit.com/@KEIRASTAVAST")
