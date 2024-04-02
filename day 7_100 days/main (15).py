7f707d7a2b54c86d21a9e7733093faf9e84e69ba
Keira Stavast <114077660+RoseThorn78@users.noreply.github.com>
2024-04-03 10:24:28 +1030

    Create main (15).py

---
+++ day 7_100 days/main (15).py
@@ -0,0 +1,12 @@
+movie = input("What is your favorite movie? ")
+if movie == "avengers":
+  print("Wow, you have great taste in movies!")
+  faveCharacter = input("Who is your favorite character? ")
+  if faveCharacter == "iron man":
+    print("Cool, iron man is a great character!")
+  else:
+    print("That character's cool, I guess...")
+elif movie == "finding nemo":
+  print("Awesome, that's a great film!")
+else:
+  print("Yeah, that's cool and all…")
