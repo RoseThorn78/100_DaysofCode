33751313ab4ed7af7ab0cf3e389497b24a852800
Keira Stavast <114077660+RoseThorn78@users.noreply.github.com>
2024-04-03 10:19:31 +1030

    Create main (14).py

---
+++ day 6_100 days/main (14).py
@@ -0,0 +1,16 @@
+print("MY SECURE LOGIN SYSTEM")
+username = input("Username > ")
+password = input("Password > ")
+print()
+if username == "keira" and password == "r07eth0rn":
+ print("Oh, hello Keira, great job on successfully signing in!")
+ print()
+ print("I'm so glad to see you here.")
+ print()
+ print("Have a wonderful day!")
+elif username == "madison" and password == "l1lz74":
+  print("Welcome, Madison! You being here has just brightened up my day!")
+elif username == "storm" and password == "p@ssw0rd":
+ print("Hey, Storm! Congrats on logging in! Have an amazing day!")
+else:
+  print("Sorry, you don't seem to have access. Please go away.")
